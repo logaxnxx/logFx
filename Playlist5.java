54c639e275fc988c75b97214b25958adc2956bd7
logan <228836377+logaxnxx@users.noreply.github.com>
2025-10-08 15:47:56 -0500

    Update Playlist5.java

--- Playlist5.java
+++ Playlist5.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 
 public class Playlist {
 	
@@ -30,6 +30,7 @@ public class Playlist {
 		Song s = new Song(line);
 		songs.add(s);
 		}
+		reader.close();
 	}
 	//getters
 	public int getNumSongs() {
@@ -43,6 +44,31 @@ public class Playlist {
 	}
 	public Song[] getSongs() {
 		return songs.toArray(new Song[0]);
+	}
+	public int[] getTotalTime() {
+		
+		int totalSeconds = 0;
+		for(Song s: songs) {
+			int[] t = s.getTime();
+			
+			if(t.length >= 1) totalSeconds += t[0];
+			if(t.length >= 2) totalSeconds += t[1] * 60;
+			if(t.length == 3) totalSeconds += t[2] * 60 * 60;
+		}
+		
+		//convert total seconds into hh:mm:ss format 
+		int hrs = totalSeconds / 3600;
+		totalSeconds -= hrs * 3600; //removes the hours from the total seconds
+		
+		int mins = totalSeconds / 60; 
+		totalSeconds -= mins * 60;
+		
+		int secs = totalSeconds;
+		
+		//returns the array
+		if(hrs > 0) return new int[] {secs, mins, hrs};
+		if(mins > 0) return new int[] {secs, mins};
+		else return new int[] {secs};
 	}
 	
 	//setters 
@@ -59,12 +85,11 @@ public class Playlist {
 	public int addSongs(Playlist playlist) {
 		if(playlist == null) return 0; 
 		//adds each element to the desired playlist
-		int added = 0;
+		int before = songs.size();
 		for(int i = 0; i < playlist.getNumSongs(); i++) {
 			songs.add(playlist.getSong(i));
-			added++;
 		}
-		return added;
+		return songs.size() - before;
 	}
 	
 	//removes songs
@@ -77,4 +102,27 @@ public class Playlist {
 		else if(songs.get(index) == null) return null;
 		return songs.remove(index);
 	}
+	
+	//save songs to a file with the to string method 
+	public void saveSongs(String filename) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+		writer.write(this.toString());
+		writer.close();
+	}
+	//calls the to string from the song class for each song in the playlist
+	public String toString() {
+		String result = "";
+		
+		for(int i = 0; i < songs.size(); i++) {
+			result = result.concat(songs.get(i).toString());
+			if(i < songs.size()-1) {
+				result = result.concat("\n"); //adds new line after each song except the last
+			}
+		}
+		
+		return result;
+	}
 }
+
+
+
